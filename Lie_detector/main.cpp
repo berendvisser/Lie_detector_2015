1d23462bf944a0e2e75f342c81e3cadc2c42503e
berendvisser <berend__visser@outlook.com>
2015-11-01 00:26:08 +0100

    Added project again, now succeeded

---
+++ Lie_detector/main.cpp
@@ -0,0 +1,36 @@
+#include "Serial.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+	int  z, value;
+	int i = 0;
+	int delay = 100;
+	z = 2;
+	char c[2];
+	//declare com port
+	string s = "COM5";
+	//convert stringtype
+	std::wstring stemp = std::wstring(s.begin(), s.end());
+	LPCWSTR sw = stemp.c_str();
+	//initialize instance serial on 'COM5'.
+	Serial serial(sw);
+
+
+
+	while (1){
+		printf("function return %d\n", serial.ReadData(c, z));
+		printf("bitvalues %d,%d\n", c[0], c[1]);
+		value = (int)c[1] + ((int)c[0] << 8);
+		printf("value : %d", value);
+		Sleep(100);
+		system("cls");
+
+
+	}
+
+	system("PAUSE");
+}
